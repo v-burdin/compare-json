3742db879fc9a4f513525faec5a54ae9333f7d7d
Mary Sitnikova <32409743+zaichik666@users.noreply.github.com>
2020-05-15 19:57:22 +0300

    Initial commit

---
+++ client/components/home.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import Head from './head'
+
+const Home = () => {
+  const [counter, setCounterNew] = useState(0)
+
+  return (
+    <div>
+      <Head title="Hello" />
+      <button type="button" onClick={() => setCounterNew(counter + 1)}>
+        updateCounter
+      </button>
+      <div> Hello World Dashboard {counter} </div>
+    </div>
+  )
+}
+
+Home.propTypes = {}
+
+export default Home
